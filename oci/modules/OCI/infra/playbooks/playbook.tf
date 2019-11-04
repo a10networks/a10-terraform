f99b0a2d260ee2728f0097c5f75eb6f64837adea
sujeetD5 <sujeet.dixit@a10networks.com>
2019-11-04 23:18:14 +0530

    code refactor

--- oci/modules/OCI/infra/playbooks/playbook.tf
+++ oci/modules/OCI/infra/playbooks/playbook.tf
@@ -63,7 +63,7 @@ resource "null_resource" "active_VT_1" {
     command = <<EOT
           sleep 6m;
 
-          ansible-playbook ../../../../modules/OCI/infra/playbooks/playbook_slb.yaml --extra-vars "a10_host='${var.vthunder_vm_public_ip}' a10_password='${element(split(".",var.password1), 4)}' app_server_IP='${var.app_server_IP}' client_primary_private_IP='${var.client_primary_private_IP}' client_primary_private_IP2='${var.client_primary_private_IP2}' virtual_server='${var.client_vip_private_ip}' server_nic_private_ip='${var.server_nic_private_IP}' next_hop_ip='${var.next_hop_ip}' mgmt_default_gateway='${var.mgmt_default_gateway}' ";
+          ansible-playbook ../../../../modules/OCI/infra/playbooks/playbook_slb.yaml --extra-vars "a10_host='${var.vthunder_vm_public_ip}' a10_password='${element(split(".",var.password1), 4)}' app_server_IP='${var.app_server_IP}' client_primary_private_IP='${var.client_primary_private_IP}' virtual_server='${var.client_vip_private_ip}' server_nic_private_ip='${var.server_nic_private_IP}' next_hop_ip='${var.next_hop_ip}' mgmt_default_gateway='${var.mgmt_default_gateway}' ";
 
           ansible-playbook ../../../../modules/OCI/infra/playbooks/playbook_vrrp.yaml --extra-vars "a10_host='${var.vthunder_vm_public_ip}' a10_password='${element(split(".",var.password1), 4)}' client_primary_private_IP='${var.client_primary_private_IP}' client_primary_private_IP2='${var.client_primary_private_IP2}' floating_client_private_ip='${var.floating_client_private_ip}'  floating_server_private_ip='${var.floating_server_private_ip}' device_id='1' ";
 
@@ -78,7 +78,7 @@ resource "null_resource" "standby_VT_1" {
   provisioner "local-exec" {
     command = <<EOT
    sleep 6m;
-    ansible-playbook ../../../../modules/OCI/infra/playbooks/playbook_slb.yaml --extra-vars "a10_host='${var.vthunder_vm_public_ip2}' a10_password='${element(split(".",var.password2), 4)}' app_server_IP='${var.app_server_IP}' client_primary_private_IP='${var.client_primary_private_IP2}' client_primary_private_IP2='${var.client_primary_private_IP}' virtual_server='${var.client_vip_private_ip}' server_nic_private_ip='${var.server_nic_private_IP2}' next_hop_ip='${var.next_hop_ip}' mgmt_default_gateway='${var.mgmt_default_gateway}' ";
+    ansible-playbook ../../../../modules/OCI/infra/playbooks/playbook_slb.yaml --extra-vars "a10_host='${var.vthunder_vm_public_ip2}' a10_password='${element(split(".",var.password2), 4)}' app_server_IP='${var.app_server_IP}' client_primary_private_IP='${var.client_primary_private_IP2}' virtual_server='${var.client_vip_private_ip}' server_nic_private_ip='${var.server_nic_private_IP2}' next_hop_ip='${var.next_hop_ip}' mgmt_default_gateway='${var.mgmt_default_gateway}' ";
 
       ansible-playbook ../../../../modules/OCI/infra/playbooks/playbook_vrrp.yaml --extra-vars "a10_host='${var.vthunder_vm_public_ip2}' a10_password='${element(split(".",var.password2), 4)}' client_primary_private_IP='${var.client_primary_private_IP2}' client_primary_private_IP2='${var.client_primary_private_IP}' floating_client_private_ip='${var.floating_client_private_ip}' floating_server_private_ip='${var.floating_server_private_ip}' device_id='2'";
 
