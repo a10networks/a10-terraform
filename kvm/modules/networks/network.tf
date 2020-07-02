c103a6a2a9630f94758f3e5af9b8dd62633d3683
Sameer Mahajan A10 <51432516+sameermahajan-a10@users.noreply.github.com>
2020-07-02 13:54:13 +0530

    network module for  KVM

---
+++ kvm/modules/networks/network.tf
@@ -0,0 +1,28 @@
+# libvirt.tf
+# add the provider
+provider "libvirt" {
+ uri = "qemu:///system"
+}
+
+variable subnets {
+type = "list"
+}
+
+variable network_names {
+type = "list"
+}
+
+
+
+resource "libvirt_network" "public" {
+    count     = "${length(var.subnets)}" #two pubic subnets- one for mgmt NIC and other for client NIC
+    name      = "${var.network_names[count.index]}"
+    bridge    = "br_${var.network_names[count.index]}"
+    mode      = "nat"
+    addresses = ["${var.subnets[count.index]}"]
+    dhcp {
+       enabled = true
+    }
+}
+
+output "public_network_name" { value = "${libvirt_network.public.*.name}"}
