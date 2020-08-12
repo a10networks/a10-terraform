43037369f3d40ef5fc007372cd921a8d563576f8
asharma-a10 <57194667+asharma-a10@users.noreply.github.com>
2020-08-12 15:30:29 -0700

    Add files via upload

--- oci/main/existing_infra/util/oci-stack.tf
+++ oci/main/existing_infra/util/oci-stack.tf
@@ -1,156 +1,156 @@
 variable "tenancy_ocid" {
-description = "Tenancy OCID"
+  description = "Tenancy OCID"
 }
 
-variable "count_vm"{
- description = "count of vm"
+variable "count_vm" {
+  description = "count of vm"
 }
 
-variable "vThunder__image_ocid"{
- description = "vThunder image OCID"
+variable "vThunder__image_ocid" {
+  description = "vThunder image OCID"
 }
 
 variable "user_ocid" {
-description = "User OCID"
+  description = "User OCID"
 }
 
 variable "compartment_id" {
-description = "Compartment OCID"
+  description = "Compartment OCID"
 }
 
 variable "dynamic_group_name" {
-description = "Dynamic group name on OCI cloud"
+  description = "Dynamic group name on OCI cloud"
 }
 
 variable "policy_name" {
-description = "Policy name on OCI cloud"
+  description = "Policy name on OCI cloud"
 }
 
 variable "app_display_name" {
-description = "app display name"
+  description = "app display name"
 }
 
 variable "region" {
-description = "Region"
+  description = "Region"
 }
 variable "private_key_path" {
-description = "Private key file path"
+  description = "Private key file path"
 }
 variable "private_key_password" {
-description = "Private Key Password"
+  description = "Private Key Password"
 }
 
 variable "fingerprint" {
-description = "fingerprint"
+  description = "fingerprint"
 }
 
 variable "vm_availability_domain" {
-description = "VM availability domain"
+  description = "VM availability domain"
 }
 
 variable "vm_display_name" {
-description = "VM display name"
+  description = "VM display name"
 }
 
 variable "vm_shape" {
-description = "VM shape"
+  description = "VM shape"
 }
 
 variable "vm_primary_vnic_display_name" {
-description = "VM primary VNIC display name"
+  description = "VM primary VNIC display name"
 }
 
 variable "vm_ssh_public_key_path" {
-description = "VM ssh public key file path"
+  description = "VM ssh public key file path"
 }
 
 variable "vm_creation_timeout" {
-description = "VM creation timeout"
+  description = "VM creation timeout"
 }
 
 variable "server_vnic_private_ip" {
-description = "server VNIC private ip"
+  description = "server VNIC private ip"
 }
 
 variable "server_vnic_display_name" {
-description = "server VNIC display name"
+  description = "server VNIC display name"
 }
 
 variable "server_vnic_index" {
-description = "server VNIC index"
+  description = "server VNIC index"
 }
 
 variable "client_vnic_display_name" {
-description = "client VNIC display name"
+  description = "client VNIC display name"
 }
 
 variable "client_vnic_index" {
-description = "client VNIC index"
+  description = "client VNIC index"
 }
 
 variable "oci_subnet_id1" {
-description = "oci_subnet_id1"
+  description = "oci_subnet_id1"
 }
 
 variable "oci_subnet_id2" {
-description = "oci_subnet_id2"
+  description = "oci_subnet_id2"
 }
 
 variable "oci_subnet_id3" {
-description = "oci_subnet_id3"
+  description = "oci_subnet_id3"
 }
 
 provider "oci" {
-  version          = ">= 3.24.0"
-  region           = "${var.region}"
-  tenancy_ocid     = "${var.tenancy_ocid}"
-  user_ocid        = "${var.user_ocid}"
-  fingerprint	   = "${var.fingerprint}"
-  private_key_path = "${var.private_key_path}"
+  version              = ">= 3.24.0"
+  region               = "${var.region}"
+  tenancy_ocid         = "${var.tenancy_ocid}"
+  user_ocid            = "${var.user_ocid}"
+  fingerprint          = "${var.fingerprint}"
+  private_key_path     = "${var.private_key_path}"
   private_key_password = "${var.private_key_password}"
-  }
+}
 
 
 module "oci_compute" {
- tenancy_ocid = "${var.tenancy_ocid}"
- compartment_id = "${var.compartment_id}"
- vThunder__image_ocid = "${var.vThunder__image_ocid}"
- count_vm = "${var.count_vm}"
- source = "../../../modules/infra/compute"
- oci_subnet_id1 = "${var.oci_subnet_id1}"
- oci_subnet_id3 = "${var.oci_subnet_id3}"
- vm_availability_domain = "${var.vm_availability_domain}"
- vm_shape = "${var.vm_shape}"
- #vm_display_name = "${var.vm_display_name}"
- vm_creation_timeout = "${var.vm_creation_timeout}"
- vm_primary_vnic_display_name = "${var.vm_primary_vnic_display_name}"
- vm_ssh_public_key_path = "${var.vm_ssh_public_key_path}"
- app_display_name = "${var.app_display_name}"
+  tenancy_ocid           = "${var.tenancy_ocid}"
+  compartment_id         = "${var.compartment_id}"
+  vThunder__image_ocid   = "${var.vThunder__image_ocid}"
+  count_vm               = "${var.count_vm}"
+  source                 = "../../../modules/infra/compute"
+  oci_subnet_id1         = "${var.oci_subnet_id1}"
+  oci_subnet_id3         = "${var.oci_subnet_id3}"
+  vm_availability_domain = "${var.vm_availability_domain}"
+  vm_shape               = "${var.vm_shape}"
+  #vm_display_name = "${var.vm_display_name}"
+  vm_creation_timeout          = "${var.vm_creation_timeout}"
+  vm_primary_vnic_display_name = "${var.vm_primary_vnic_display_name}"
+  vm_ssh_public_key_path       = "${var.vm_ssh_public_key_path}"
+  app_display_name             = "${var.app_display_name}"
 }
 
 
 module "dynamic_group" {
-  source = "../../../modules/infra/dynamic_group"
-  instance_list = "${concat([module.oci_compute.instance_id_active], module.oci_compute.instance_list)}"
-  tenancy_ocid = "${var.tenancy_ocid}"
-  compartment_id = "${var.compartment_id}"
+  source             = "../../../modules/infra/dynamic_group"
+  instance_list      = "${concat([module.oci_compute.instance_id_active], module.oci_compute.instance_list)}"
+  tenancy_ocid       = "${var.tenancy_ocid}"
+  compartment_id     = "${var.compartment_id}"
   dynamic_group_name = "${var.dynamic_group_name}"
-  policy_name = "${var.policy_name}"
+  policy_name        = "${var.policy_name}"
 }
 
 module "nic" {
- #source = "../../../modules/OCI/infra/NIC"
- source = "../../../modules/infra/NIC"
- oci_subnet_id2 = "${var.oci_subnet_id2}"
- compartment_id = "${var.compartment_id}"
- server_vnic_display_name = "${var.server_vnic_display_name}"
- #server_vnic_private_ip = "${var.server_vnic_private_ip}"
- instance_list = "${module.oci_compute.instance_list}"
- instance_id_active = "${module.oci_compute.instance_id_active}"
- #instance_id = "${module.oci_compute.instance_id}"
- oci_subnet_id3 = "${var.oci_subnet_id3}"
- client_vnic_display_name = "${var.client_vnic_display_name}"
- #client_vnic_private_ip = "${var.client_vnic_private_ip}"
+  #source = "../../../modules/OCI/infra/NIC"
+  source                   = "../../../modules/infra/NIC"
+  oci_subnet_id2           = "${var.oci_subnet_id2}"
+  compartment_id           = "${var.compartment_id}"
+  server_vnic_display_name = "${var.server_vnic_display_name}"
+  #server_vnic_private_ip = "${var.server_vnic_private_ip}"
+  instance_list      = "${module.oci_compute.instance_list}"
+  instance_id_active = "${module.oci_compute.instance_id_active}"
+  #instance_id = "${module.oci_compute.instance_id}"
+  oci_subnet_id3           = "${var.oci_subnet_id3}"
+  client_vnic_display_name = "${var.client_vnic_display_name}"
+  #client_vnic_private_ip = "${var.client_vnic_private_ip}"
 }
 
-output "vnic_ID" {value = "${module.nic.vnic_id}" }
+output "vnic_ID" { value = "${module.nic.vnic_id}" }
--- oci/main/existing_infra/util/terraform.tfvars
+++ oci/main/existing_infra/util/terraform.tfvars
@@ -1,36 +1,43 @@
 #Provider details
-tenancy_ocid = ""
-user_ocid = ""
-compartment_id = ""
-region = "" # region for the instance. e.g: us-ashburn-1
+tenancy_ocid   = "ocid1.tenancy.oc1..aaaaaaaag4vgboeufnpjup6yzug7bmu7oqg7mypie2civld36hcykym2upta"
+user_ocid      = "ocid1.user.oc1..aaaaaaaarcli6dmsb36i2jxcmblu4jcyiuwahunn6ajxquzljk4fm3x6ww6q"
+compartment_id = "ocid1.compartment.oc1..aaaaaaaawayicdhc6dxp5ay7iqc7gvonb7oanxqb4ufhjne3xc37d6637ona"
+region         = "us-ashburn-1" # region for the instance. e.g: us-ashburn-1
 
 #Login details
-private_key_path = ""
-private_key_password = ""
-fingerprint = ""
+private_key_path     = "/root/aditya/.oci/oci_key_asharma.pem"
+private_key_password = "password"
+fingerprint          = "0d:16:2b:2b:ce:66:60:d8:0e:4e:12:db:2d:ce:ca:16"
 
 #vThunder VM details
-vm_display_name = "TF-vThunder"
-vm_availability_domain = ""
-vm_shape = "VM.Standard2.8"
-vm_creation_timeout = "5m"
+vm_display_name              = "TF-vThunder"
+vm_availability_domain       = "paEo:US-ASHBURN-AD-2"
+vm_shape                     = "VM.Standard2.8"
+vm_creation_timeout          = "5m"
 vm_primary_vnic_display_name = "primary-vnic"
-vm_ssh_public_key_path = ""
-vThunder__image_ocid=""
+vm_ssh_public_key_path       = "/root/aditya/.ssh/id_rsa.pub"
+
+
+dynamic_group_name = "dynamic-group-name-test1"
+policy_name        = "policy-name-test1"
+
+vThunder__image_ocid = "ocid1.image.oc1.iad.aaaaaaaa23bsqdaajxcnwjptcecpkyaiae6zmkocj3xxfksncrh6pii4mnva"
+
 
 #Secondary VNIC details - server
-server_vnic_private_ip = ""  #Server vnic private ip
+server_vnic_private_ip   = "" #Server vnic private ip
 server_vnic_display_name = "server"
-server_vnic_index = "1"
+server_vnic_index        = "1"
 
 #Secondary VNIC details - client
-client_vnic_private_ip = ""  #Client vnic private ip
+client_vnic_private_ip   = "" #Client vnic private ip
 client_vnic_display_name = "client"
-client_vnic_index = "2"
+client_vnic_index        = "2"
 
 app_display_name = "app"
-oci_subnet_id1 = ""  # first subnet id
-oci_subnet_id2 = ""  # second subnet id
-oci_subnet_id3 = ""  # third subnet id
 
-count_vm = "1"
+oci_subnet_id1 = "ocid1.subnet.oc1.iad.aaaaaaaaebkdu77o7ifiq2ulo3p3dmgs6jg3py6oi67ixgrdjrqahrj5aeoq" # first subnet id
+oci_subnet_id2 = "ocid1.subnet.oc1.iad.aaaaaaaaqwx2xncanxwjvu5t54acdph4emqpafolmqb2npep653dnkbgkf7a" # second subnet id
+oci_subnet_id3 = "ocid1.subnet.oc1.iad.aaaaaaaaj5keb7vgozzqs4fcwrj4vow43m2f2gci5lietirsi3kzabkjau6a" # third subnet id
+
+count_vm = "2"
