0dbf281b02c6eea498e10ecaf6d1cd428444adef
asharma-a10 <57194667+asharma-a10@users.noreply.github.com>
2020-08-12 15:00:04 -0700

    Adding OCI-instance spin-up and tear-down wrappers

---
+++ oci/main/existing_infra/util/a10-spin-oci-instance.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+terraform init
+terraform plan
+terraform apply --auto-approve
--- oci/main/existing_infra/util/oci-stack.tf
+++ oci/main/existing_infra/util/oci-stack.tf
@@ -2,12 +2,28 @@ variable "tenancy_ocid" {
 description = "Tenancy OCID"
 }
 
+variable "count_vm"{
+ description = "count of vm"
+}
+
+variable "vThunder__image_ocid"{
+ description = "vThunder image OCID"
+}
+
 variable "user_ocid" {
 description = "User OCID"
 }
 
 variable "compartment_id" {
 description = "Compartment OCID"
+}
+
+variable "dynamic_group_name" {
+description = "Dynamic group name on OCI cloud"
+}
+
+variable "policy_name" {
+description = "Policy name on OCI cloud"
 }
 
 variable "app_display_name" {
@@ -94,30 +110,47 @@ provider "oci" {
   private_key_password = "${var.private_key_password}"
   }
 
+
 module "oci_compute" {
-tenancy_ocid = "${var.tenancy_ocid}"
-compartment_id = "${var.compartment_id}"
- source = "../../../modules/OCI/infra/compute"
+ tenancy_ocid = "${var.tenancy_ocid}"
+ compartment_id = "${var.compartment_id}"
+ vThunder__image_ocid = "${var.vThunder__image_ocid}"
+ count_vm = "${var.count_vm}"
+ source = "../../../modules/infra/compute"
  oci_subnet_id1 = "${var.oci_subnet_id1}"
  oci_subnet_id3 = "${var.oci_subnet_id3}"
  vm_availability_domain = "${var.vm_availability_domain}"
  vm_shape = "${var.vm_shape}"
- vm_display_name = "${var.vm_display_name}"
+ #vm_display_name = "${var.vm_display_name}"
  vm_creation_timeout = "${var.vm_creation_timeout}"
  vm_primary_vnic_display_name = "${var.vm_primary_vnic_display_name}"
  vm_ssh_public_key_path = "${var.vm_ssh_public_key_path}"
  app_display_name = "${var.app_display_name}"
 }
 
+
+module "dynamic_group" {
+  source = "../../../modules/infra/dynamic_group"
+  instance_list = "${concat([module.oci_compute.instance_id_active], module.oci_compute.instance_list)}"
+  tenancy_ocid = "${var.tenancy_ocid}"
+  compartment_id = "${var.compartment_id}"
+  dynamic_group_name = "${var.dynamic_group_name}"
+  policy_name = "${var.policy_name}"
+}
+
 module "nic" {
- source = "../../../modules/OCI/infra/NIC"
+ #source = "../../../modules/OCI/infra/NIC"
+ source = "../../../modules/infra/NIC"
  oci_subnet_id2 = "${var.oci_subnet_id2}"
+ compartment_id = "${var.compartment_id}"
  server_vnic_display_name = "${var.server_vnic_display_name}"
- server_vnic_private_ip = "${var.server_vnic_private_ip}"
- instance_id = "${module.oci_compute.instance_id}"
+ #server_vnic_private_ip = "${var.server_vnic_private_ip}"
+ instance_list = "${module.oci_compute.instance_list}"
+ instance_id_active = "${module.oci_compute.instance_id_active}"
+ #instance_id = "${module.oci_compute.instance_id}"
  oci_subnet_id3 = "${var.oci_subnet_id3}"
  client_vnic_display_name = "${var.client_vnic_display_name}"
  #client_vnic_private_ip = "${var.client_vnic_private_ip}"
 }
 
-output "vnic ID" {value = "${module.nic.vnic_id}" }
+output "vnic_ID" {value = "${module.nic.vnic_id}" }
